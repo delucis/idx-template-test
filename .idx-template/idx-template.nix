8f5230bd9f779039ebdbcc48586b75131282e4e4
Chris Swithinbank <swithinbank@gmail.com>
2024-11-22 19:10:50 +0100

    Initial test config

---
+++ .idx-template/idx-template.nix
@@ -0,0 +1,28 @@
+{pkgs, template ? "basics", version ? "latest", packageManager ? "npm", typescript ? "strict", git ? true, ... }: {
+  packages = [
+    pkgs.nodejs_20
+    pkgs.yarn
+    pkgs.nodePackages.pnpm
+    pkgs.bun
+    pkgs.j2cli
+    pkgs.nixfmt
+  ];
+
+  bootstrap = ''
+    mkdir "$out"
+    ${
+      if packageManager == "npm" then "npm create astro@${version} \"$out\" -- --template ${template} --typescript ${typescript} ${if git then "--git" else "--no-git" } --no-install"
+      else ""
+    }
+
+    mkdir -p "$out"/.idx
+    packageManager=${packageManager} j2 ${./devNix.j2} -o "$out"/.idx/dev.nix
+    nixfmt "$out"/.idx/dev.nix
+    cp ./icon.png "$out"/.idx/icon.png
+
+    ${
+      if packageManager == "npm" then "( cd \$out && npm i --package-lock-only --ignore-scripts )"
+      else ""
+    }
+  '';
+}
